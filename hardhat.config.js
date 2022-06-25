21f4f4ab4eceed5cd35b171ecad05b75f893b200
wighawag <wighawag@gmail.com>
2022-06-25 22:15:25 +0100

    0.8.15

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require('hardhat-deploy');
 const {addForkConfiguration, node_url, accounts} = require('./utils/network')
 
 module.exports = {
-    solidity: "0.8.13",
+    solidity: "0.8.15",
 
     networks: addForkConfiguration({
       localhost: {
