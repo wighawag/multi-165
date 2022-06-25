2c003044e738ce9ed80ede01ea18f1b95a5ead9b
wighawag <wighawag@gmail.com>
2022-06-25 22:21:08 +0100

    0.8.13

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require('hardhat-deploy');
 const {addForkConfiguration, node_url, accounts} = require('./utils/network')
 
 module.exports = {
-    solidity: "0.8.15",
+    solidity: "0.8.13",
 
     networks: addForkConfiguration({
       localhost: {
--- scripts/failcase.s.sol
+++ scripts/failcase.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
+pragma solidity 0.8.13;
 
 import "forge-std/Script.sol";
 import "src/Multi165.sol";
