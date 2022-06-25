f2a9403db0a7cf98cbd1a40262d4222921f7df52
wighawag <wighawag@gmail.com>
2022-06-25 18:00:03 +0100

    MIT code

--- src/Multi165.sol
+++ src/Multi165.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 interface IERC165 {function supportsInterface(bytes4 interfaceId) external view returns (bool);}
